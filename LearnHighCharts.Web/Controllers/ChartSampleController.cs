295f634e71f7b29073eec6e9b386bc4e9dc7016b
bgreer5050@gmail.com <bgreer5050@gmail.com>
2016-05-12 06:42:39 -0500

    Added ChartSample controller and view

---
+++ LearnHighCharts.Web/Controllers/ChartSampleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearnHighCharts.Web.Controllers
+{
+    public class ChartSampleController : Controller
+    {
+        // GET: ChartSample
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
